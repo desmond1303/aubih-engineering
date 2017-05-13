ae1b2d654c6a0bcd979f0e3fa55c2243b3657a55
Dino Prašo <dinopraso@Dinos-MacBook-Pro.local>
2017-05-13 11:21:14 +0200

    Added Footer

--- app/components/page-footer.js
+++ app/components/page-footer.js
@@ -3,4 +3,42 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   classNames: ['footer-view'],
 
+  usefulLinks: [
+    {
+      title: 'About AUBiH',
+      link: '#',
+    }, {
+      title: 'AUBiH Online',
+      link: '#',
+    }, {
+      title: 'AUBiH TV',
+      link: '#',
+    }, {
+      title: 'AUBiH Online Store',
+      link: '#',
+    }, {
+      title: 'Contact Us',
+      link: '#',
+    },
+  ],
+
+  latestNews: [
+    {
+      title: 'BIT Alliance and AUBiH Cooperation',
+      timestamp: '17th April, 2017',
+      img: '',
+      link: '#',
+    }, {
+      title: 'Agreement on cooperation',
+      timestamp: '12th April, 2017',
+      img: '',
+      link: '#',
+    }, {
+      title: 'Our reform of Education',
+      timestamp: '7th April, 2017',
+      img: '',
+      link: '#',
+    },
+  ],
+
 });
