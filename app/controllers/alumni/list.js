5423059287710cad8b0f58b24be55d57015df1e0
Dino <desmond1303@gmail.com>
2017-05-15 16:24:02 +0200

    Added Alumni

--- app/controllers/alumni/list.js
+++ app/controllers/alumni/list.js
@@ -1,6 +1,29 @@
 import Ember from 'ember';
 import Constants from './../../configuration/constants';
 
+const {
+  computed,
+} = Ember;
+
 export default Ember.Controller.extend({
-  alumni: Constants.main.alumni.list,
+
+  alumni: computed(function () {
+    return Constants.main.alumni.list.sort(this.compareNames);
+  }),
+
+  compareNames: function (a, b) {
+    let nameOne = a.name.toLowerCase();
+    let nameTwo = b.name.toLowerCase();
+
+    if (nameOne < nameTwo) {
+      return -1;
+    }
+
+    if (nameOne > nameTwo) {
+      return 1;
+    }
+
+    return 0;
+  },
+
 });
