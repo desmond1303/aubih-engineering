f91c93e93e59c660784640285f8cb207c1f8d817
Dino <desmond1303@gmail.com>
2017-05-17 17:06:57 +0200

    Add Full Navigation

--- app/configuration/main-navigation.js
+++ app/configuration/main-navigation.js
@@ -225,23 +225,646 @@ export default [
   }, {
     title: 'Academics',
     link: 'index',
-    subMenu: [],
+    subMenu: [
+      {
+        title: 'Academic Affairs',
+        links: [
+          {
+            title: 'Overview',
+            link: 'index',
+          }, {
+            title: 'Academic Provost\'s Message',
+            link: 'index',
+          }, {
+            title: 'Diplomas and Programs',
+            link: 'index',
+          }, {
+            title: 'Library Services',
+            link: 'index',
+          }, {
+            title: 'Accreditation',
+            link: 'index',
+          }, {
+            title: 'Catalog 2017 - ENG',
+            link: 'index',
+          }, {
+            title: 'Catalog 2017 - BOS',
+            link: 'index',
+          },
+        ],
+      }, {
+        title: 'Programs',
+        links: [
+          {
+            title: 'International Sales and Marketing',
+            link: 'index',
+          }, {
+            title: 'International Finance',
+            link: 'index',
+          }, {
+            title: 'Master of Business Administration',
+            link: 'index',
+          }, {
+            title: 'Management',
+            link: 'index',
+          }, {
+            title: 'Information Technology',
+            link: 'index',
+          }, {
+            title: 'Cyber Security',
+            link: 'index',
+          }, {
+            title: 'Software Engineering',
+            link: 'index',
+          }, {
+            title: 'System Engineering',
+            link: 'index',
+          }, {
+            title: 'Computer Engineering',
+            link: 'index',
+          }, {
+            title: 'Law',
+            link: 'index',
+          }, {
+            title: 'International and European Public Law',
+            link: 'index',
+          }, {
+            title: 'International Law',
+            link: 'index',
+          }, {
+            title: 'International Relations',
+            link: 'index',
+          }, {
+            title: 'International Economy',
+            link: 'index',
+          }, {
+            title: 'International Business Law',
+            link: 'index',
+          }, {
+            title: 'National and International Security',
+            link: 'index',
+          }, {
+            title: 'Industrial Technology Management',
+            link: 'index',
+          }, {
+            title: 'Graphic and Multimedia Design',
+            link: 'index',
+          },
+        ],
+      }, {
+        title: 'Colleges',
+        links: [
+          {
+            title: 'College of Economics',
+            link: 'index',
+          }, {
+            title: 'College of Information Technology',
+            link: 'index',
+          }, {
+            title: 'College of Engineering',
+            link: 'index',
+          }, {
+            title: 'College of International Relations',
+            link: 'index',
+          }, {
+            title: 'College of Security and Defense',
+            link: 'index',
+          }, {
+            title: 'College of Industrial Technology',
+            link: 'index',
+          }, {
+            title: 'American Academy of Modern Arts',
+            link: 'index',
+          }, {
+            title: 'AUBiH Online',
+            link: 'index',
+          },
+        ],
+      }, {
+        title: 'Academic Services',
+        links: [
+          {
+            title: 'Overview',
+            link: 'index',
+          }, {
+            title: 'V.P. Academic Services Message',
+            link: 'index',
+          }, {
+            title: 'Academic Advisor\'s Message',
+            link: 'index',
+          }, {
+            title: 'Academic Advising and Counselling',
+            link: 'index',
+          }, {
+            title: 'Student Academic Program Rules',
+            link: 'index',
+          }, {
+            title: 'Academic Calendar',
+            link: 'index',
+          }, {
+            title: 'Event Calendar',
+            link: 'index',
+          }, {
+            title: 'International Programs',
+            link: 'index',
+          }, {
+            title: 'English Language Preparation Course',
+            link: 'index',
+          },
+        ],
+      },
+    ],
   }, {
     title: 'Degree',
     link: 'index',
-    subMenu: [],
+    subMenu: [
+      {
+        title: 'Overview',
+        link: 'index',
+        links: [
+          {
+            title: 'Programs',
+            link: 'index',
+          }, {
+            title: 'Bachelor Admission',
+            link: 'index',
+          }, {
+            title: 'Master Admission',
+            link: 'index',
+          }, {
+            title: 'Doctoral Admission',
+            link: 'index',
+          }, {
+            title: 'Diplomas and Programs',
+            link: 'index',
+          }, {
+            title: 'Academic Calendar',
+            link: 'index',
+          }, {
+            title: 'Student Handbook 2016-2017',
+            link: 'index',
+          }, {
+            title: 'Entrance Exam',
+            link: 'index',
+          }, {
+            title: 'Tuition and Fees',
+            link: 'index',
+          }, {
+            title: 'Accreditation',
+            link: 'index',
+          }, {
+            title: 'Catalog 2017 - ENG',
+            link: 'index',
+          }, {
+            title: 'Catalog 2017 - BOS',
+            link: 'index',
+          },
+        ],
+      }, {
+        title: 'Bachelor',
+        link: 'index',
+        links: [
+          {
+            title: 'International Sales and Marketing',
+            link: 'index',
+          }, {
+            title: 'International Finance',
+            link: 'index',
+          }, {
+            title: 'International Technology',
+            link: 'index',
+          }, {
+            title: 'Software Engineering',
+            link: 'index',
+          }, {
+            title: 'Law',
+            link: 'index',
+          }, {
+            title: 'International Relations',
+            link: 'index',
+          }, {
+            title: 'National and International Security',
+            link: 'index',
+          }, {
+            title: 'Industrial Technology Management',
+            link: 'index',
+          }, {
+            title: 'Graphic and Multimedia Design',
+            link: 'index',
+          },
+        ],
+      }, {
+        title: 'Master',
+        link: 'index',
+        links: [
+          {
+            title: 'International Sales and Marketing',
+            link: 'index',
+          }, {
+            title: 'Master of Business Administration',
+            link: 'index',
+          }, {
+            title: 'Cyber Security',
+            link: 'index',
+          }, {
+            title: 'Software Engineering',
+            link: 'index',
+          }, {
+            title: 'System Engineering',
+            link: 'index',
+          }, {
+            title: 'International and European Public Law',
+            link: 'index',
+          }, {
+            title: 'International Relations',
+            link: 'index',
+          }, {
+            title: 'International Economy and Trade',
+            link: 'index',
+          }, {
+            title: 'National and International Security',
+            link: 'index',
+          }, {
+            title: 'Industrial Technology Management',
+            link: 'index',
+          },
+        ],
+      }, {
+        title: 'Doctoral',
+        link: 'index',
+        links: [
+          {
+            title: 'Management',
+            link: 'index',
+          }, {
+            title: 'Cyber Security',
+            link: 'index',
+          }, {
+            title: 'Computer Engineering',
+            link: 'index',
+          }, {
+            title: 'International Law',
+            link: 'index',
+          }, {
+            title: 'International Business Law',
+            link: 'index',
+          }, {
+            title: 'National and International Security',
+            link: 'index',
+          }, {
+            title: 'Industrial Technology Management',
+            link: 'index',
+          },
+        ],
+      },
+    ],
   }, {
     title: 'Students',
     link: 'index',
-    subMenu: [],
+    subMenu: [
+      {
+        title: 'Students Affairs',
+        link: 'index',
+        links: [
+          {
+            title: 'Overview',
+            link: 'index',
+          }, {
+            title: 'V.P. Student Services Message',
+            link: 'index',
+          }, {
+            title: 'Currnt Students',
+            link: 'index',
+          }, {
+            title: 'Student Handbook 2016-2017',
+            link: 'index',
+          }, {
+            title: 'Future Students',
+            link: 'index',
+          }, {
+            title: 'Academic Calendar',
+            link: 'index',
+          }, {
+            title: 'Event Calendar',
+            link: 'index',
+          }, {
+            title: 'Graduation and Commencement',
+            link: 'index',
+          }, {
+            title: 'Student Consumer Information',
+            link: 'index',
+          }, {
+            title: '10 Reasons to Study at AUBiH',
+            link: 'index',
+          }, {
+            title: 'Catalog 2017 - ENG',
+            link: 'index',
+          }, {
+            title: 'Catalog 2017 - BOS',
+            link: 'index',
+          },
+        ],
+      }, {
+        title: 'Students Services',
+        link: 'index',
+        links: [
+          {
+            title: 'Guest Lecutres',
+            link: 'index',
+          }, {
+            title: 'Housing',
+            link: 'index',
+          }, {
+            title: 'Excursions & Study Visits',
+            link: 'index',
+          }, {
+            title: 'Social & Cultural Activities',
+            link: 'index',
+          }, {
+            title: 'Work and Travel Program (USA)',
+            link: 'index',
+          }, {
+            title: 'Student Services',
+            link: 'index',
+          }, {
+            title: 'Study Abroad Services Program',
+            link: 'index',
+          }, {
+            title: 'Record and Registration',
+            link: 'index',
+          }, {
+            title: 'Career Development Services',
+            link: 'index',
+          },
+        ],
+      }, {
+        title: 'Students Activities',
+        link: 'index',
+        links: [
+          {
+            title: 'Student Life',
+            link: 'index',
+          }, {
+            title: 'Student Government',
+            link: 'index',
+          }, {
+            title: 'Student Research',
+            link: 'index',
+          }, {
+            title: 'Student Clubs and Organizations',
+            link: 'index',
+          }, {
+            title: 'Sports and Recreation',
+            link: 'index',
+          },
+        ],
+      }, {
+        title: 'Students Stories',
+        link: 'index',
+        links: [
+          {
+            title: 'AUBiH Ambassadors',
+            link: 'index',
+          }, {
+            title: 'Discover AUBiH',
+            link: 'index',
+          },
+        ],
+      },
+    ],
   }, {
     title: 'Admission',
     link: 'index',
-    subMenu: [],
+    subMenu: [
+      {
+        title: 'Enrollment',
+        link: 'index',
+        links: [
+          {
+            title: 'Overview',
+            link: 'index',
+          }, {
+            title: 'V.P. Enrollment Message',
+            link: 'index',
+          }, {
+            title: 'Bachelor Admission',
+            link: 'index',
+          }, {
+            title: 'Master Admission',
+            link: 'index',
+          }, {
+            title: 'Doctoral Admission',
+            link: 'index',
+          }, {
+            title: 'Visiting Students Admission',
+            link: 'index',
+          }, {
+            title: 'American School of English Admission',
+            link: 'index',
+          }, {
+            title: 'Accreditation',
+            link: 'index',
+          }, {
+            title: 'Student Consumer Information',
+            link: 'index',
+          }, {
+            title: 'Catalog 2017 - ENG',
+            link: 'index',
+          }, {
+            title: 'Catalog 2017 - BOS',
+            link: 'index',
+          },
+        ],
+      }, {
+        title: 'Freshman Students',
+        link: 'index',
+        links: [
+          {
+            title: 'Freshman Undergraduate Students',
+            link: 'index',
+          }, {
+            title: 'Gaduate and Postgraduate Students',
+            link: 'index',
+          }, {
+            title: 'Transfer Students Admission',
+            link: 'index',
+          }, {
+            title: 'Request for Evaluation of Courses',
+            link: 'index',
+          }, {
+            title: 'Apply Now',
+            link: 'index',
+          },
+        ],
+      }, {
+        title: 'Tuition and Fees',
+        link: 'index',
+        links: [
+          {
+            title: 'Tuition and Fees',
+            link: 'index',
+          }, {
+            title: 'Late Fee',
+            link: 'index',
+          }, {
+            title: 'Refund Policy',
+            link: 'index',
+          },
+        ],
+      }, {
+        title: 'Language Requirements',
+        link: 'index',
+        links: [
+          {
+            title: 'Entrance Exam',
+            link: 'index',
+          }, {
+            title: 'Preparation Classes',
+            link: 'index',
+          },
+        ],
+      },
+    ],
   }, {
     title: 'Global',
     link: 'index',
-    subMenu: [],
+    subMenu: [
+      {
+        title: 'About',
+        link: 'index',
+        links: [
+          {
+            title: 'Overview',
+            link: 'index',
+          }, {
+            title: 'International Advisory Board',
+            link: 'index',
+          }, {
+            title: 'V.P. International Relations Message',
+            link: 'index',
+          }, {
+            title: 'Accreditation',
+            link: 'index',
+          }, {
+            title: 'Credit Transfers / Transfer Students',
+            link: 'index',
+          }, {
+            title: 'Student Testimonials',
+            link: 'index',
+          }, {
+            title: 'Catalog 2017 - ENG',
+            link: 'index',
+          }, {
+            title: 'Catalog 2017 - BOS',
+            link: 'index',
+          }, {
+            title: 'Catalog - Summer Programs',
+            link: 'index',
+          }, {
+            title: 'Contact us',
+            link: 'index',
+          },
+        ],
+      }, {
+        title: 'Programs',
+        links: [
+          {
+            title: 'International Sales and Marketing',
+            link: 'index',
+          }, {
+            title: 'International Finance',
+            link: 'index',
+          }, {
+            title: 'Master of Business Administration',
+            link: 'index',
+          }, {
+            title: 'Management',
+            link: 'index',
+          }, {
+            title: 'Information Technology',
+            link: 'index',
+          }, {
+            title: 'Cyber Security',
+            link: 'index',
+          }, {
+            title: 'Software Engineering',
+            link: 'index',
+          }, {
+            title: 'System Engineering',
+            link: 'index',
+          }, {
+            title: 'Computer Engineering',
+            link: 'index',
+          }, {
+            title: 'Law',
+            link: 'index',
+          }, {
+            title: 'International and European Public Law',
+            link: 'index',
+          }, {
+            title: 'International Law',
+            link: 'index',
+          }, {
+            title: 'International Relations',
+            link: 'index',
+          }, {
+            title: 'International Economy',
+            link: 'index',
+          }, {
+            title: 'International Business Law',
+            link: 'index',
+          }, {
+            title: 'National and International Security',
+            link: 'index',
+          }, {
+            title: 'Industrial Technology Management',
+            link: 'index',
+          }, {
+            title: 'Graphic and Multimedia Design',
+            link: 'index',
+          },
+        ],
+      }, {
+        title: 'Study Abroad',
+        link: 'index',
+        links: [
+          {
+            title: 'Overview',
+            link: 'index',
+          }, {
+            title: 'Visas',
+            link: 'index',
+          }, {
+            title: 'Housing',
+            link: 'index',
+          }, {
+            title: 'Health Information',
+            link: 'index',
+          }, {
+            title: 'Travel Information',
+            link: 'index',
+          }, {
+            title: 'Tourist Information',
+            link: 'index',
+          }, {
+            title: 'Other Usefull Information',
+            link: 'index',
+          },
+        ],
+      }, {
+        title: 'Summer Programs',
+        link: 'index',
+        links: [
+          {
+            title: 'Overview',
+            link: 'index',
+          }, {
+            title: 'Peace and Conflict Program',
+            link: 'index',
+          },
+        ],
+      },
+    ],
   }, {
     title: 'Career',
     link: 'index',
