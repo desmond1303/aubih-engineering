7d84cdea62722b8d5a9e5a28a804f2537ccef61a
Dino <desmond1303@gmail.com>
2017-05-28 22:21:57 +0200

    Banner Background

--- app/components/in-text-banner.js
+++ app/components/in-text-banner.js
@@ -1,5 +1,22 @@
 import Ember from 'ember';
 
+const {
+  computed,
+  String: {
+    htmlSafe,
+  },
+} = Ember;
+
 export default Ember.Component.extend({
   classNames: ['in-text-banner-view'],
+
+  image: null,
+
+  backgroundImage: computed('image', function () {
+    let image = this.get('image');
+    if (image) {
+      return htmlSafe('background-image: url(' + this.get('image') + ')');
+    }
+  }),
+
 });
