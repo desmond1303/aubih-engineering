5f890fd116a5cfff208d972ebc0b7c77109cf67b
Dino <desmond1303@gmail.com>
2017-06-02 09:00:06 +0200

    Fix Alumni Nav

--- app/configuration/sub-navigation/alumni.js
+++ app/configuration/sub-navigation/alumni.js
@@ -1,15 +1,9 @@
 export default [
   {
-    title: 'Alumni',
-    link: 'alumni',
-    subMenu: [
-      {
-        title: 'About',
-        link: 'alumni.index',
-      }, {
-        title: 'List',
-        link: 'alumni.list',
-      },
-    ],
+    title: 'About',
+    link: 'alumni.index',
+  }, {
+    title: 'List',
+    link: 'alumni.list',
   },
 ];
