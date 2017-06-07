c952e91a82c8d8df04871ada205472d4aebb529e
Dino <desmond1303@gmail.com>
2017-06-07 20:18:30 +0200

    Add Alumni Quotes

--- app/configuration/alumni/list.js
+++ app/configuration/alumni/list.js
@@ -14,22 +14,25 @@ export default [
     company: 'Google',
     profession: 'Online Media Associate',
     photo: '/assets/images/alumni/agovic-damir.jpg',
-    quote: 'Lorem Ipsum Dolor sit Amet Forever Lorem Ipsum Dolor sit Amet Forever Lorem Ipsum Dolor sit Amet Forever',
+    quote: 'What matters is the opportunity to work in an international environment, and that the educational institution focuses on international cooperation.',
   }, {
     name: 'Mlivo Harun',
     company: 'Google',
     profession: 'Digital Transformation Account Manager',
     photo: '/assets/images/alumni/mlivo-harun.jpg',
+    quote: 'During the study I learned what exactly big companies are looking for, how to prove to them and how to present yourselves so that the best ones are interested in you.',
   }, {
     name: 'Camdžić Naida',
     company: 'European Bank for Reconstruction and Development',
     profession: 'Analyist',
     photo: '/assets/images/alumni/camdzic-naida.jpg',
+    quote: 'The knowledge acquired through my education has helped me immensely. The approach to work, team work, and communication skills has defined my success in everyday work with my clients.',
   }, {
     name: 'Brkan Amar',
     company: 'CDS Consulting',
     profession: 'Business Development Specialist',
     photo: '/assets/images/alumni/amar-brkan.jpg',
+    quote: 'After a month since graduation I have found the job of an economic consultant at USAID PHARE project',
   }, {
     name: 'Idrizović Kenan',
     company: 'American University in Bosnia and Herzegovina',
@@ -40,16 +43,19 @@ export default [
     company: 'Development Bank of Federation of Bosnia and Herzegovina',
     profession: 'Credit Analyst',
     photo: '/assets/images/alumni/asceric-damir.jpg',
+    quote: 'Knowledge and skills acquired at AUBiH helped my brief professional development. Currently, I am a director of Development Bank of Federation of Bosnia and Herzegovina.',
   }, {
     name: 'Isabegović Selma',
     company: 'Klaur International',
     profession: 'Economist',
     photo: '/assets/images/alumni/isabegovic-selma.jpg',
+    quote: 'The economics from AUBiHhas offered me the opportunity to present myself the best and on a global level.',
   }, {
     name: 'Pap Amar',
     company: 'American University in Bosnia and Herzegovina',
     profession: 'Career Development Center Director',
     photo: '/assets/images/alumni/pap-amar.jpg',
+    quote: 'Thanks to the AUBiH Career Development Center, I’m already half way there to become a competent employee.',
   }, {
     name: 'Bajrektarević Faruk',
     company: 'ECHO Technology Solutions',
@@ -60,6 +66,7 @@ export default [
     company: 'Prevent Group BiH',
     profession: 'IT Key User',
     photo: '/assets/images/alumni/placeholder.png',
+    quote: 'Education is the only capital that is worth investing in, without feeling guilty of doing so.',
   }, {
     name: 'Catić Dženad',
     company: 'Alternativa Ltd.',
